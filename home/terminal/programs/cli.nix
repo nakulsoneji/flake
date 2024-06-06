286b03eab3073b707849e08ebb9bb1c01dd41b57
nakulsoneji <nakulsoneji@gmail.com>
2024-06-05 19:19:49 -0700

    changed zsh config

--- home/terminal/programs/cli.nix
+++ home/terminal/programs/cli.nix
@@ -13,6 +13,21 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    colors = {
+      bg = "#1e1e2e";
+      "bg+" = "#313244";
+      hl = "#f38ba8";
+      "hl+" = "#f38ba8";
+      fg = "#cdd6f4";
+      "fg+" = "#cdd6f4";
+
+      spinner = "#f5e0dc";
+      header = "#f38ba8";
+      info = "#cba6f7";
+      pointer = "#f5e0dc";
+      marker = "#f5e0dc";
+      prompt = "#cba6f7";
+    };
   };
 
   programs.ripgrep = {
