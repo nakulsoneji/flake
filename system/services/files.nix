3a6103d93acd46cc0c99a9876331ee9f4d41ca73
nakulsoneji <nakulsoneji@gmail.com>
2024-06-10 20:10:09 -0700

    added more services for files

--- system/services/files.nix
+++ system/services/files.nix
@@ -1,4 +1,11 @@
-{
+{pkgs, ...}: {
   services.gvfs.enable = true;
   services.tumbler.enable = true;
+  # for ios devices https://nixos.wiki/wiki/IOS
+  services.usbmuxd.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    libimobiledevice
+    ifuse
+  ];
 }
